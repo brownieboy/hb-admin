e04a402a2109bab4a05d1df8ea8dc7074e859198
Michael Brown <brownieboy@gmail.com>
2018-02-25 10:44:00 +1100

    Basic Yup validation on new Stages screen

--- src/components/stage-form.js
+++ src/components/stage-form.js
@@ -1,12 +1,21 @@
 // Render Prop
 import React from "react";
 import { Formik, Field } from "formik";
+import yup from "yup";
 
-const StageForm = () => (
+const StageForm = ({
+  fieldValues = { name: "", id: "", sortOrder: -1 },
+  editExisting = false
+}) => (
   <div>
     <h1>My Form</h1>
     <Formik
-      initialValues={{ name: "", id: "", sortOrder: -1 }}
+      initialValues={Object.assign({}, fieldValues)}
+      validationSchema={yup.object().shape({
+        id: yup.string().required(),
+        name: yup.string().required(),
+        sortOrder: yup.number().required().positive().integer()
+      })}
       onSubmit={(values, actions) => {
         setTimeout(() => {
           alert(JSON.stringify(values, null, 2));
@@ -20,17 +29,19 @@ const StageForm = () => (
             name="id"
             placeholder="Stage id (must be unique, no spaces)"
           />
-
+          {props.errors.id && <div id="feedback">{props.errors.id}</div>}
           <br />
           <Field type="text" name="name" placeholder="Stage name" />
+          {props.errors.name && <div id="feedback">{props.errors.name}</div>}
+
           <br />
           <Field
             type="number"
             name="sortOrder"
             placeholder="Sort order (integer)"
           />
+          {props.errors.sortOrder && <div id="feedback">{props.errors.sortOrder}</div>}
 
-          {props.errors.name && <div id="feedback">{props.errors.name}</div>}
           <button type="submit">Submit</button>
         </form>
       )}
