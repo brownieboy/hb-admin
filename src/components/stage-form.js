a99ca40f13b12b86e4d3ef128024a5b662549f46
Michael Brown <brownieboy@gmail.com>
2018-02-25 08:48:02 +1100

    Basic Formik form with no validation

--- src/components/stage-form.js
+++ src/components/stage-form.js
@@ -1,86 +1,37 @@
 // Render Prop
 import React from "react";
-import { Formik } from "formik";
+import { Formik, Field } from "formik";
 
 const StageForm = () => (
   <div>
-    <h1>Stage Form</h1>
-    {/*
-      The benefit of the render prop approach is that you have full access to React's
-      state, props, and composition model. Thus there is no need to map outer props
-      to values...you can just set the initial values, and if they depend on props / state
-      then--boom--you can directly access to props / state.
-
-      The render prop accepts your inner form component, which you can define separately or inline
-      totally up to you:
-      - `<Formik render={props => <form>...</form>}>`
-      - `<Formik component={InnerForm}>`
-      - `<Formik>{props => <form>...</form>}</Formik>` (identical to as render, just written differently)
-    */}
+    <h1>My Form</h1>
     <Formik
-      initialValues={{
-        id: "",
-        name: "",
-        sortOrder: -1
+      initialValues={{ name: "", id: "", sortOrder: -1 }}
+      onSubmit={(values, actions) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          actions.setSubmitting(false);
+        }, 200);
       }}
-      validate={values => {
-        // same as above, but feel free to move this into a class method now.
-        let errors = {};
-        if (!values.email) {
-          errors.email = "Required";
-        } else if (
-          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
-        ) {
-          errors.email = "Invalid email address";
-        }
-        return errors;
-      }}
-      onSubmit={(
-        values,
-        { setSubmitting, setErrors /* setValues and other goodies */ }
-      ) => {
-        LoginToMyApp(values).then(
-          user => {
-            setSubmitting(false);
-            // do whatevs...
-            // props.updateUser(user)
-          },
-          errors => {
-            setSubmitting(false);
-            // Maybe transform your API's errors into the same shape as Formik's
-            setErrors(errors);
-          }
-        );
-      }}
-      render={({
-        values,
-        errors,
-        touched,
-        handleChange,
-        handleBlur,
-        handleSubmit,
-        isSubmitting
-      }) => (
-        <form onSubmit={handleSubmit}>
-          <input
-            type="email"
-            name="email"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.email}
+      render={props => (
+        <form onSubmit={props.handleSubmit}>
+          <Field
+            type="text"
+            name="id"
+            placeholder="Stage id (must be unique, no spaces)"
           />
-          {touched.email && errors.email && <div>{errors.email}</div>}
-          <input
-            type="password"
-            name="password"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.password}
+
+          <br />
+          <Field type="text" name="name" placeholder="Stage name" />
+          <br />
+          <Field
+            type="number"
+            name="sortOrder"
+            placeholder="Sort order (integer)"
           />
-          {touched.password && errors.password && <div>{errors.password}</div>}
-          <button type="submit" disabled={isSubmitting}>
-            Submit
-          </button>
+
+          {props.errors.name && <div id="feedback">{props.errors.name}</div>}
+          <button type="submit">Submit</button>
         </form>
       )}
     />
