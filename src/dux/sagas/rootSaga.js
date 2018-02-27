469736f4a072017e1eb74526e9343bd9e2e5d213
Michael Brown <brownieboy@gmail.com>
2018-02-28 07:42:11 +1100

    Adding band write sagas...

--- src/dux/sagas/rootSaga.js
+++ src/dux/sagas/rootSaga.js
@@ -2,7 +2,7 @@ import { all } from "redux-saga/effects";
 
 import readFirebaseDataSagas from "./readFirebaseDataSagas.js";
 import loginFirebaseSagas from "./loginFirebaseSagas.js";
-import writeFirebaseSagas from "./writeFirebaseSagas.js";
+import writeFirebaseStagesSagas from "./writeFirebaseStagesSagas.js";
 
 // Combine sagas solution taken from Andarist's comment at:
 // https://github.com/redux-saga/redux-saga/issues/160
