969b880b2100024dc271fcbcf37537c5774304d5
Michael Brown <brownieboy@gmail.com>
2018-03-03 12:16:29 +1100

    Updated dates saga & reducer

--- src/dux/datesReducer.js
+++ src/dux/datesReducer.js
@@ -4,6 +4,18 @@ const LOAD_DATES_NOW = "LOAD_DATES_NOW"; // Imperative, hence "NOW"!
 const FETCH_DATES_REQUEST = "FETCH_DATES_REQUEST";
 const FETCH_DATES_SUCCESS = "FETCH_DATES_SUCCESS";
 const FETCH_DATES_FAILURE = "FETCH_DATES_FAILURE";
+const SAVE_EDITED_DATES = "SAVE_EDITED_DATES";
+const SAVE_DATES_REQUEST = "SAVE_DATES_REQUEST";
+const SAVE_DATES_SUCCESS = "SAVE_DATES_SUCCESS";
+const SAVE_DATES_FAILED = "SAVE_DATES_FAILED";
+
+export const actionTypes = {
+  SAVE_NEW_DATES,
+  SAVE_EDITED_DATES,
+  SAVE_DATES_REQUEST,
+  SAVE_DATES_SUCCESS,
+  SAVE_DATES_FAILED
+};
 
 // Reducer
 const datesReducer = (
@@ -43,8 +55,35 @@ const setFetchDatesFailed = errorMessage => ({
 export const datesDuxActions = {
   setFetchDatesFailed,
   setFetchDatesRequest,
-  setFetchDatesSucceeded
+  setFetchDatesSucceeded,
+    saveDateRequest,
+  saveDateSucceeded,
+  saveNewDate,
+  saveDateFailed
 };
+
+export const saveNewDate = datesList => ({
+  type: SAVE_NEW_DATES,
+  payload: datesList
+});
+
+export const saveEditedDate = datesList => ({
+  type: SAVE_EDITED_DATES,
+  payload: datesList
+});
+
+export const saveDateRequest = () => ({
+  type: SAVE_DATES_REQUEST
+});
+
+export const saveDateSucceeded = () => ({
+  type: SAVE_DATES_SUCCESS
+});
+
+export const saveDateFailed = error => ({
+  type: SAVE_DATES_FAILED,
+  payload: error
+});
 
 
 export default datesReducer;
