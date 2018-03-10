e6c6542af204f019255b1e3906ef86eaf29bd967
Michael Brown <brownieboy@gmail.com>
2018-03-10 14:59:03 +1100

    Now sending percentage of uploads to Redux store correctly

--- src/dux/sagas/uploadFirebaseImagesSagas.js
+++ src/dux/sagas/uploadFirebaseImagesSagas.js
@@ -1,4 +1,5 @@
 import { call, put, select, takeEvery } from "redux-saga/effects";
+import { eventChannel } from "redux-saga";
 import { reduxSagaFirebase } from "../../apis/firebase.js";
 
 import {
@@ -18,91 +19,33 @@ function* syncFileUrl(filePath) {
   }
 }
 
+function* handleEventEmit(snapshot) {
+  // yield console.log("saga uploadCardImage started, snapshot:");
+  // yield console.log(snapshot);
+  const progress = snapshot.bytesTransferred * 100 / snapshot.totalBytes;
+  yield put(
+    storageDuxActions.updateStorageThumbStatus({
+      percentUploaded: progress
+    })
+  );
+}
+
 function* uploadCardImage(data) {
-  // yield put(saveBandRequest());
-  yield console.log("saga uploadCardImage started, data=");
-  yield console.log(data);
+  // yield console.log("saga handleEventEmit started, data=");
+  // yield console.log(data);
 
   const file = yield data.payload.fileInfo;
   const filePath = yield `${globalTypes.STORAGE.THUMBS_PATH}/${file.name}`;
-  console.log("filePath=");
-  console.log(filePath);
-  console.log("file=");
-  console.log(file);
-
-  const tempStorage = reduxSagaFirebase.storage;
 
   const task = reduxSagaFirebase.storage.uploadFile(filePath, file);
 
-  task.on("state_changed", snapshot => {
-    const pct = snapshot.bytesTransferred * 100 / snapshot.totalBytes;
-    console.log(`${pct}%`);
-  });
+  const channel = eventChannel(emit => task.on("state_changed", emit));
+  yield takeEvery(channel, handleEventEmit);
 
   yield task;
   yield syncFileUrl(filePath);
 
-  // task.on("state_changed", snapshot => {
-  //   const pct = snapshot.bytesTransferred * 100 / snapshot.totalBytes;
-  //   console.log(`${pct}%`);
-  // });
-
-  /*
-  const storageRef = yield firebaseApp.storage().ref();
-  const uploadTask = storageRef
-    .child(`${globalTypes.STORAGE.THUMBS_PATH}/${file.name}`)
-    .put(file, { contentType: file.type });
-
-  uploadTask.on(
-    "state_changed",
-    snapshot => {
-      console.log("upload Task state_changed");
-      const progress = snapshot.bytesTransferred / snapshot.totalBytes * 100;
-      put(
-        storageDuxActions.updateStorageThumbStatus({
-          percentUploaded: progress
-        })
-      );
-    },
-    error => {
-      console.log("File upload error: " + error);
-    },
-    () => {
-      // Handle successful uploads on complete
-      // For instance, get the download URL: https://firebasestorage.googleapis.com/...
-      console.log("uploadTask success");
-      const downloadUrl = uploadTask.snapshot.downloadURL;
-      put(storageDuxActions.updateStorageThumbStatus({ downloadUrl }));
-    }
-  );
-  */
-
-  // const bandsList = yield select(state => state.bandsState.bandsList);
-
-  // const ref = yield firebaseApp.database().ref(globalTypes.DATABASE.BANDS_PATH);
-
-  // The put statements didn't trigger Redux when I had them instead the .then()
-  // and .catch() statements.  So I set a variable inside the .catch() then refer
-  // to it in the if statement after the ref has run.  Clunky, but it works.
-  // let firebaseError = "";
-  // yield ref.set(bandsList).catch(e => {
-  //   firebaseError = e;
-  //   console.log("Firebase band save error=" + e);
-  // });
-
-  // if (firebaseError === "") {
-  //   yield put(saveBandSucceeded());
-  // } else {
-  //   yield put(saveBandFailed(firebaseError));
-  // }
 }
-
-// const bandsConfigObj = {
-//   path: globalTypes.DATABASE.BANDS_PATH,
-//   requestAction: saveBandRequest,
-//   successAction: saveBandSucceeded,
-//   failAction: saveBandFailed
-// };
 
 const uploadFirebaseImagesSagas = [
   takeEvery(storageActionTypes.SEND_STORAGE_THUMB_START, uploadCardImage)
