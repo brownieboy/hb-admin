d5f7e750b465009141fcf232006b593ef3726c5d
Michael Brown <brownieboy@gmail.com>
2018-03-12 16:16:22 +1100

    Added car image to bands form but not plumbed in...

--- src/components/band-form.js
+++ src/components/band-form.js
@@ -149,37 +149,102 @@ class BandForm extends Component {
 
                   <Button type="submit">Submit</Button>
                 </form>
+                <hr />
                 <h2>Images</h2>
-                <div name="imagesWrapper">
-                  <Label for="thumbInput">Thumbnail image:</Label>
+                {!isEditExisting && (
                   <div>
-                    <input
-                      type="file"
-                      name="thumbInput"
-                      onChange={this.handleThumbFileChange}
-                    />
+                    You must save this form at least once before you can add
+                    images.
                   </div>
-                  <button
-                    disabled={!this.state.thumbFileInfo.name}
-                    onClick={() => {
-                      sendStorageThumbStart({
-                        bandId: values.id,
-                        fileInfo: this.state.thumbFileInfo
-                      });
-                    }}
-                  >
-                    Upload thumbnail
-                  </button>
-                  <div style={{ maxWidth: 200 }}>
-                    <div className="text-center">
-                      {parseInt(thumbProgress, 10)}%
+                )}
+                <div name="imagesWrapper" style={{ marginBottom: 100 }}>
+                  <div name="thumbImagesWrapper" style={{ display: "flex" }}>
+                    <div name="imagesLeftWrapper">
+                      <Label for="thumbInput">Thumbnail image:</Label>
+                      <div>
+                        <input
+                          type="file"
+                          disabled={!isEditExisting}
+                          name="thumbInput"
+                          onChange={this.handleThumbFileChange}
+                        />
+                      </div>
+                      <button
+                        disabled={!this.state.thumbFileInfo.name}
+                        onClick={() => {
+                          sendStorageThumbStart({
+                            bandId: values.id,
+                            fileInfo: this.state.thumbFileInfo
+                          });
+                        }}
+                      >
+                        Upload thumbnail
+                      </button>
+                      <div
+                        style={{
+                          maxWidth: 200,
+                          display: this.state.thumbFileInfo.name
+                            ? "block"
+                            : "none"
+                        }}
+                      >
+                        <div className="text-center">
+                          {parseInt(thumbProgress, 10)}%
+                        </div>
+                        <Progress
+                          name="thumbProgressBar"
+                          value={thumbProgress}
+                          color={thumbProgress === 100 ? "success" : "info"}
+                        />
+                      </div>
                     </div>
-                    <Progress
-                      name="thumbProgressBar"
-                      value={thumbProgress}
-                      color={thumbProgress === 100 ? "success" : "info"}
-                    />
-                    <ThumbImage url={values.thumbFullUrl} />
+                    <div name="imagesRightWrapper">
+                      <ThumbImage url={values.thumbFullUrl} />
+                    </div>
+                  </div>
+                  <div name="cardImagesWrapper" style={{ display: "flex" }}>
+                    <div name="imagesLeftWrapper">
+                      <Label for="cardInput">Thumbnail image:</Label>
+                      <div>
+                        <input
+                          type="file"
+                          disabled={!isEditExisting}
+                          name="cardInput"
+                          onChange={this.handleThumbFileChange}
+                        />
+                      </div>
+                      <button
+                        disabled={!this.state.cardFileInfo.name}
+                        onClick={() => {
+                          sendStorageThumbStart({
+                            bandId: values.id,
+                            fileInfo: this.state.cardFileInfo
+                          });
+                        }}
+                      >
+                        Upload card
+                      </button>
+                      <div
+                        style={{
+                          maxWidth: 200,
+                          display: this.state.cardFileInfo.name
+                            ? "block"
+                            : "none"
+                        }}
+                      >
+                        <div className="text-center">
+                          {parseInt(cardProgress, 10)}%
+                        </div>
+                        <Progress
+                          name="cardProgressBar"
+                          value={cardProgress}
+                          color={cardProgress === 100 ? "success" : "info"}
+                        />
+                      </div>
+                    </div>
+                    <div name="imagesRightWrapper">
+                      <ThumbImage url={values.cardFullUrl} />
+                    </div>
                   </div>
                 </div>
               </div>
