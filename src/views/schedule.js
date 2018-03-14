da4a8d60cc6eff8761500a734321eef7a126d903
Michael Brown <brownieboy@gmail.com>
2018-03-14 16:54:31 +1100

    Starting to group appearances data by day...

--- src/views/schedule.js
+++ src/views/schedule.js
@@ -10,40 +10,50 @@ import { LoadStatusIndicator } from "../components/loadsaveindicator.js";
 // const getAppearanceKey = (bandId, stageId, dateTimeStart) =>
 //   `${bandId}~${stageId}~${dateTimeStart}`;
 
-const listSchedule = (
-  appearancesArray,
-  getBandInfoForId,
-  getStageInfoForId
-) => {
-  let bandInfo, stageInfo;
-  // console.log("appearancesArray start = " + appearancesArray);
-  return appearancesArray.map(appearanceMember => {
-    const { bandId, stageId } = appearanceMember;
-    bandInfo = getBandInfoForId(bandId);
-    stageInfo = getStageInfoForId(stageId);
-    // console.log("bandInfo=" + JSON.stringify(bandInfo, null, 4));
-    // console.log("stageInfo=" + JSON.stringify(stageInfo, null, 4));
-    return (
-      <ListGroupItem key={appearanceMember.id}>
-        {`${bandInfo.name} (${stageInfo.name})`}
-        <Link to={`/scheduleform/${appearanceMember.id}`}>
-          <i className="icon-pencil" />
-        </Link>
-      </ListGroupItem>
-    );
-  });
-};
+// const listSchedule = (
+//   appearancesArray,
+//   getBandInfoForId,
+//   getStageInfoForId
+// ) => {
+//   let bandInfo, stageInfo;
+//   // console.log("appearancesArray start = " + appearancesArray);
+//   return appearancesArray.map(appearanceMember => {
+//     const { bandId, stageId } = appearanceMember;
+//     bandInfo = getBandInfoForId(bandId);
+//     stageInfo = getStageInfoForId(stageId);
+//     // console.log("bandInfo=" + JSON.stringify(bandInfo, null, 4));
+//     // console.log("stageInfo=" + JSON.stringify(stageInfo, null, 4));
+//     return (
+//       <ListGroupItem key={appearanceMember.id}>
+//         {`${bandInfo.name} (${stageInfo.name})`}
+//         <Link to={`/scheduleform/${appearanceMember.id}`}>
+//           <i className="icon-pencil" />
+//         </Link>
+//       </ListGroupItem>
+//     );
+//   });
+// };
 
 // ListItem key={`${bandId}${dateTimeStart}`}
 
+const getAppearancesListDayLevel = groupedDayData =>
+  groupedDayData.map(dayMember => [
+    <ListGroupItem key={dayMember.key}>
+      <span style={{ fontWeight: "bold" }}>{dayMember.key.toUpperCase()}</span>
+    </ListGroupItem>,
+    <div key={`${dayMember.key}-stagewrapper`} style={{ marginBottom: 20 }}>
+      Line values
+    </div>
+  ]);
+
 const Schedule = ({
-  appearancesListByDateTime,
+  // appearancesListByDateTime,
   appearancesGroupedByDayThenStage,
   fetchStatus,
-  fetchError,
-  appearancesWithBandAndStageNames,
-  getBandInfoForId,
-  getStageInfoForId
+  fetchError
+  // appearancesWithBandAndStageNames,
+  // getBandInfoForId,
+  // getStageInfoForId
 }) => {
   //   console.log("In Schedule, appearancesListByDateTime:");
   // console.log(appearancesListByDateTime);
@@ -56,7 +66,7 @@ const Schedule = ({
       <h1>Schedule</h1>
       <LoadStatusIndicator fetchStatus={fetchStatus} fetchError={fetchError} />
       <ListGroup>
-      List group would be here
+        {getAppearancesListDayLevel(appearancesGroupedByDayThenStage)}
       </ListGroup>
       <Link to="/scheduleform">Add appearance</Link>
     </div>
@@ -78,10 +88,13 @@ Schedule.propTypes = {
   // appearancesWithBandAndStageNames: PropTypes.arrayOf(
   //   PropTypes.object.isRequired
   // ).isRequired,
+  appearancesGroupedByDayThenStage: PropTypes.arrayOf(
+    PropTypes.object.isRequired
+  ).isRequired,
   fetchStatus: PropTypes.string.isRequired,
-  fetchError: PropTypes.string.isRequired,
-  getBandInfoForId: PropTypes.func.isRequired,
-  getStageInfoForId: PropTypes.func.isRequired
+  fetchError: PropTypes.string.isRequired
+  // getBandInfoForId: PropTypes.func.isRequired,
+  // getStageInfoForId: PropTypes.func.isRequired
 };
 
 export default Schedule;
