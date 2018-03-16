8dc1686b13876225cd1bcd1f4e50d45d38b20373
Michael Brown <brownieboy@gmail.com>
2018-03-16 17:26:06 +1100

    First dummy publishedData going to Firebase

--- src/dux/sagas/writeFirebasePublishSagas.js
+++ src/dux/sagas/writeFirebasePublishSagas.js
@@ -1,7 +1,9 @@
 import { put, takeEvery } from "redux-saga/effects";
 import {
   actionTypes as publishActionTypes,
-  savePublishRequest
+  savePublishRequest,
+  savePublishSucceeded,
+  savePublishFailed
 } from "../publishReducer.js";
 import firebaseApp from "../../apis/firebase.js";
 
@@ -10,28 +12,28 @@ import { types as globalTypes } from "../../constants/firebasePaths.js";
 function* saveData(action) {
   // Every saved edit, we write back to Firebase as an array.
   yield put(savePublishRequest());
-  // const homeList = yield select(state => state.homeState.homeList);
+  // const PublishList = yield select(state => state.PublishState.PublishList);
   console.log(
     "saveData saga handler for SAVE_PUBLISH_NOW, action=" +
       JSON.stringify(action, null, 4)
   );
 
-  // const ref = yield firebaseApp.database().ref(globalTypes.DATABASE.HOME_PAGE_PATH);
+  const ref = yield firebaseApp.database().ref(globalTypes.DATABASE.PUBLISHED_DATA_PATH);
 
   // The put statements didn't trigger Redux when I had them instead the .then()
   // and .catch() statements.  So I set a variable inside the .catch() then refer
   // to it in the if statement after the ref has run.  Clunky, but it works.
-  // let firebaseError = "";
-  // yield ref.set(action.payload).catch(e => {
-  //   firebaseError = e;
-  //   console.log("Firebase home save error=" + e);
-  // });
+  let firebaseError = "";
+  yield ref.set(action.payload).catch(e => {
+    firebaseError = e;
+    console.log("Firebase Publish save error=" + e);
+  });
 
-  // if (firebaseError === "") {
-  //   yield put(saveHomeSucceeded());
-  // } else {
-  //   yield put(saveHomeFailed(firebaseError));
-  // }
+  if (firebaseError === "") {
+    yield put(savePublishSucceeded());
+  } else {
+    yield put(savePublishFailed(firebaseError));
+  }
 }
 
 const writeFirebaseSagas = [
