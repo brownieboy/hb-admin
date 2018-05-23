2b89e6035db6b48ed52ba743cf94c79776def840
Michael Brown <brownieboy@gmail.com>
2018-05-24 07:43:27 +1000

    Fixed missing HelstonburyFBID

--- src/containers/contactus-form-conn.js
+++ src/containers/contactus-form-conn.js
@@ -42,6 +42,8 @@ const mapStateToPropsEdit = state => ({
   email1: state.contactUsState.contactUs.email1,
   email2: state.contactUsState.contactUs.email2,
   helstonburyWebUrl: state.contactUsState.contactUs.helstonburyWebUrl,
+    helstonburyFBID:
+    state.contactUsState.contactUs.helstonburyFBID,
   helstonburyMerchandiseFBID:
     state.contactUsState.contactUs.helstonburyMerchandiseFBID,
   helstonburyMerchandiseFBText:
